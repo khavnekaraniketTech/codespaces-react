d311229c628067cac0ad2f19baf26c1b89a8d23d
khavnekaraniketTech <aniketkhavnekar.test@gmail.com>
2026-04-29 20:46:11 +0000

    normalcheckups

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className="flex items-center justify-center h-screen bg-white">
+    <h1 className='text-3xl font-bold text-center text-red-500'>hello Aniiket</h1>
     </div>
-  );
+    );  
 }
 
 export default App;
